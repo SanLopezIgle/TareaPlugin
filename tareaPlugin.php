016da9bd829fb3647b4eaa3ab98301fda5f8c232
Sandra Lopez <slopeziglesias@danielcastelao.org>
2023-12-12 14:10:13 +0100

    creando el nuevo plugin, contador de palabras

--- tareaPlugin.php
+++ tareaPlugin.php
@@ -1,36 +1,110 @@
 <?php
-/**
- * @package Hello_Words
- * @version 1.0.0
- */
+
 /*
-Plugin Name: Tarea PLugin
+Plugin Name: Examen Plugin
 Plugin URI: http://wordpress.org/plugins/tarea-plugin/
-Description: Esto sirve para cambiar unas palabras por otras
+Description: Esto sirve para contar las palabras de una publicacion
 Author: Sandra Lopez
 Version: 1.0.0
 Author URI: http://Sandra.Lopez/
 */
 
-// Define las palabras a reemplazar y sus sustitutos
-function word_replacer_get_words() {
-    return array(
-        'palabra1' => 'sustituto1',
-        'palabra2' => 'sustituto2',
-        'palabra3' => 'sustituto3',
-        'palabra4' => 'sustituto4',
-        'palabra5' => 'sustituto5',
+
+// Funcion para contar las palabras de una publicación
+function word_count($content) {
+    $post_id = get_the_ID(); // obtener id del post actual
+    if (is_single() || is_page()) {
+        $contador_palabras = str_word_count(strip_tags($content)); // Cuenta las palabras
+        // strip_tags() elimina las etiquetas html para asi devolver el texto sin etiquetas
+
+        guardar_conteo_palabras($contador_palabras, $post_id);
+
+        $texto_palabra = ($contador_palabras == 1) ? 'palabra' : 'palabras'; // Dependiendo de si es 1 o más palabras, escribo en singular o plural
+
+        $contador_html = '<p>Esta publicación tiene ' . $contador_palabras . ' ' . $texto_palabra . ' sin contar el titulo.</p>';
+        $content .= $contador_html;
+        // Escribo al final de la publicación la frase de arriba con las palabras que esta contiene
+    }
+
+    return $content;
+}
+
+
+
+// Engancha la función al hook 'the_content'
+add_filter('the_content', 'word_count');
+
+
+function iniciarPlugin (){
+    createTable();
+    //insertData();
+    selectData();
+}
+
+// Creamos nuestra tabla con los datos que nos interesan
+function createTable(){
+    global $wpdb;
+
+    $table_name = $wpdb->prefix . 'mi_tabla';
+
+    $charset_collate = $wpdb->get_charset_collate();
+
+    $sql = "CREATE TABLE $table_name (
+        id mediumint(9) NOT NULL AUTO_INCREMENT,
+        numero_palabras int NOT NULL,
+        post_id int NOT MULL,
+        PRIMARY KEY (id)
+    ) $charset_collate;";
+
+
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    dbDelta( $sql );
+}
+
+function selectData() {
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'mi_tabla';
+
+    $resultados = $wpdb->get_results("SELECT * FROM $table_name");
+
+    return $resultados;
+}
+
+// Funcion para almacenar el conteo de palabras
+function guardar_conteo_palabras($contador_palabras, $post_id){
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'mi_tabla';
+
+    $wpdb->replace(
+        $table_name,
+        array(
+            'numero_palabras' => $contador_palabras,
+            'post_id' => $post_id
+        ),
+        array(
+            '%d',
+            '%d'
+        )
     );
 }
 
-// Función para reemplazar las palabras en el contenido del post
-function word_replacer_replace_words($text) {
-    $words_to_replace = word_replacer_get_words();
-    foreach ($words_to_replace as $word => $replacement) {
-        $text = str_ireplace($word, $replacement, $text);
-    }
-    return $text;
+// Función para insertar datos en la base de datos
+function insertData($post_id, $contador_palabras) {
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'mi_tabla';
+
+    $wpdb->insert(
+        $table_name,
+        array(
+            'post_id' => $post_id,
+            'numero_palabras' => $contador_palabras
+        ),
+        array(
+            '%d',
+            '%d'
+        )
+    );
 }
 
-// Engancha la función al hook 'the_content'
-add_filter('the_content', 'word_replacer_replace_words');
+add_action('plugin_loaded', 'iniciarPlugin');
+
